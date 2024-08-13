630b0f2e32433e097aa8081d9cabd69a1c112f25
Giliane Rochat <giliane.rochat@students.unibe.ch>
2024-08-13 16:47:55 +0200

    Optimised data structure for E-SINDy input

--- NFB_esindy_debug.jl
+++ NFB_esindy_debug.jl
@@ -24,8 +24,6 @@ end
 begin
 	# SciML tools
 	import ModelingToolkit, Symbolics
-
-	
 	
 	# Standard libraries
 	using Statistics, Plots, CSV, DataFrames, Printf
@@ -49,8 +47,6 @@ This notebook is about estimating the equation describing the unknown dynamics a
 begin
 	#Pkg.develop(Pkg.PackageSpec(name="DataDrivenDiffEq", uuid="5C42544C-9741-4B26-99DF-F196E0D3E510"))
 	#Pkg.develop(Pkg.PackageSpec(name="DataDrivenSparse", uuid="5b588203-7d8b-4fab-a537-c31a7f73f46a"))
-	
-	
 end
 
 # ╔═╡ d6b1570f-b98b-4a98-b5e5-9d747930a5ab
@@ -105,7 +101,37 @@ begin
 	df4 = CSV.read("./Data/ngf_highCC_10m.csv", DataFrame)
 	df5 = CSV.read("./Data/ngf_highCC_3m_3v.csv", DataFrame)
 	df6 = CSV.read("./Data/ngf_lowCC_10m_10v.csv", DataFrame)
-	df_NGF = vcat(df1, df3, df4, df5, df6)#[1:4000, :]
+	df_NGF = vcat(df1, df3, df4, df5, df6)
+end
+
+# ╔═╡ f21876f0-3124-40ad-ad53-3a53efe77040
+function create_nfb_data(df, labels = [], smoothing=0.)
+
+	# Retrieve the number of samples
+	n_samples = sum(df.time .== 0)
+	size_sample = Int(nrow(df) / n_samples)
+
+	time = df.time
+	X = [df.g1p_fit df.g2p_fit df.Xact_fit]
+	GT = (0.75 .* df.PFB_fit .* 
+		(1 .- df.Raf_fit) ./ (0.01 .+ (1 .- df.Raf_fit)))
+	Y = df.NN_approx
+
+	if smoothing != 0
+		smoothed_Y = zeros(size(Y))
+		for i in 0:(n_samples-1)
+			x_t = df.time[1 + i*size_sample: (i+1)*size_sample]
+			y = df.NN_approx[1 + i*size_sample: (i+1)*size_sample]
+			λ = smoothing  # smoothing parameter
+			spl = fit(SmoothingSpline, x_t, y, λ)
+			smoothed_y = predict(spl)
+			smoothed_Y[1 + i*size_sample: (i+1)*size_sample] = smoothed_y
+		end
+	end
+
+	@assert size(X, 1) == size(Y, 1)
+		
+	return (time=time, X=X, Y=smoothed_Y, GT=GT, labels=labels)
 end
 
 # ╔═╡ 6c7929f5-15b2-4e19-8c26-e709f0da182e
@@ -114,76 +140,32 @@ function create_gf_data(df, labels = [], smoothing=0.)
 	# Retrieve the number of samples
 	n_samples = sum(df.time .== 0)
 	size_sample = Int(nrow(df) / n_samples)
-	
+
+	time = df.time
 	X = [df.Raf_fit df.PFB_fit]
+	GT = (0.75 .* df.PFB_fit .* 
+		(1 .- df.Raf_fit) ./ (0.01 .+ (1 .- df.Raf_fit)))
 	Y = df.NN_approx
-	smoothed_Y = zeros(size(Y))
-	for i in 0:(n_samples-1)
-		x_t = df.time[1 + i*size_sample: (i+1)*size_sample]
-		y = df.NN_approx[1 + i*size_sample: (i+1)*size_sample]
-		λ = smoothing  # smoothing parameter
-		spl = fit(SmoothingSpline, x_t, y, λ)
-		smoothed_y = predict(spl)
-		smoothed_Y[1 + i*size_sample: (i+1)*size_sample] = smoothed_y
+
+	if smoothing != 0
+		smoothed_Y = zeros(size(Y))
+		for i in 0:(n_samples-1)
+			x_t = df.time[1 + i*size_sample: (i+1)*size_sample]
+			y = df.NN_approx[1 + i*size_sample: (i+1)*size_sample]
+			λ = smoothing
+			spl = fit(SmoothingSpline, x_t, y, λ)
+			smoothed_y = predict(spl)
+			smoothed_Y[1 + i*size_sample: (i+1)*size_sample] = smoothed_y
+		end
 	end
 
 	@assert size(X, 1) == size(Y, 1)
-	@assert size(Y) == size(smoothed_Y)
 		
-	return (X=X, Y=smoothed_Y, labels=labels)
+	return (time=time, X=X, Y=smoothed_Y, GT=GT, labels=labels)
 end
 
 # ╔═╡ 5f0fb956-7c84-4ff2-944f-e9ddfccab748
-ngf_data = create_gf_data(df_NGF, ["High CC 10'/10'" "High CC 3'/20'"], 300.)
-
-# ╔═╡ a23630a3-d0e0-4f7d-9cfc-aeda021d54c1
-begin
-	w1 = (0.75 .* df_NGF.PFB_fit .* 
-		(1 .- df_NGF.Raf_fit) ./ (0.01 .+ (1 .- df_NGF.Raf_fit)))
-	x1 = df_NGF.time[1:801]
-	y1 = df_NGF.NN_approx[1:801]
-	lambda = 0.  # smoothing parameter
-	spl1 = fit(SmoothingSpline, x1, y1, lambda)
-	smoothed_y1 = predict(spl1)
-	plot(x1, smoothed_y1)
-	plot!(x1, y1[1:801])
-end
-
-# ╔═╡ 5092c181-85ee-438c-9f0e-52c1f8c284dd
-begin
-	w2 = (0.75 .* df2.PFB_fit .* 
-		(1 .- df2.Raf_fit) ./ (0.01 .+ (1 .- df2.Raf_fit)))
-	x2 = df2.time
-	y2 = df2.NN_approx
-	spl2 = fit(SmoothingSpline, x2, y2, lambda)
-	smoothed_y2 = predict(spl2)
-	plot(x2, smoothed_y2)
-	plot!(x2, w2)
-end
-
-# ╔═╡ f81bdacb-f5d1-40de-9f63-29f6d8934d95
-begin
-	w3 = (0.75 .* df3.PFB_fit .* 
-		(1 .- df3.Raf_fit) ./ (0.01 .+ (1 .- df3.Raf_fit)))
-	x3 = df3.time
-	y3 = df3.NN_approx
-	spl3 = fit(SmoothingSpline, x3, y3, lambda)
-	smoothed_y3 = predict(spl3)
-	plot(x3, smoothed_y3)
-	plot!(x3, w3)
-end
-
-# ╔═╡ 3b439203-21dc-42e4-91f6-57e05fa3bc12
-begin
-	w4 = (0.75 .* df4.PFB_fit .* 
-		(1 .- df4.Raf_fit) ./ (0.01 .+ (1 .- df4.Raf_fit)))
-	x4 = df4.time
-	y4 = df4.NN_approx
-	spl4 = fit(SmoothingSpline, x4, y4, lambda)
-	smoothed_y4 = predict(spl4)
-	plot(x4, smoothed_y4)
-	plot!(x4, w4)
-end
+ngf_data = create_gf_data(df_NGF, ["High CC 10'/10'" "High CC 3'/20'" "High CC 10'" "High CC 3'/3'" "Low CC 10'/10'"], 300.)
 
 # ╔═╡ 74ad0ae0-4406-4326-822a-8f3e027077b3
 md"""
@@ -210,48 +192,70 @@ end
 begin
 	# Define a basis of functions to estimate the unknown equation of GFs model
 	gf_h = DataDrivenDiffEq.polynomial_basis(x[1:2], 2)
-	gf_basis = DataDrivenDiffEq.Basis([gf_h; gf_h .* i], x[1:2], implicits=i)
+	gf_basis = DataDrivenDiffEq.Basis([gf_h; gf_h .* i], x[1:2], implicits=i[1:1])
 end
 
 # ╔═╡ 4646bfce-f8cc-4d24-b461-753daff50f71
 begin
-	sampler = DataDrivenDiffEq.DataProcessing(split = 0.8, shuffle = true, batchsize = 100)
+	sampler = DataDrivenDiffEq.DataProcessing(split = 0.8, shuffle = true, batchsize = 30)
 	options = DataDrivenDiffEq.DataDrivenCommonOptions(data_processing = sampler, digits=1, abstol=1e-10, reltol=1e-10, denoise=true)
+end
+
+# ╔═╡ 9c4ade80-d5ad-40b4-bf57-53479bb8a74b
+ngf_dd_prob = DataDrivenDiffEq.DirectDataDrivenProblem(ngf_data.X', ngf_data.Y')
+
+# ╔═╡ e488c35e-d454-4942-b08e-599d27f00e17
+res_dd = DataDrivenDiffEq.solve(
+			ngf_dd_prob, 
+			gf_basis, 
+			ImplicitOptimizer(DataDrivenSparse.SR3(0.001, 10)), 
+			options = options)
+
+# ╔═╡ 1ec3d1b5-83d9-4093-8c1f-e726441743a6
+coef = res_dd.out
+
+# ╔═╡ d7ffe014-f4bf-4236-a553-d0dd6f06f146
+begin
+	metric_bic = []
+	metric_dof = []
 end
 
 # ╔═╡ 034f5422-7259-42b8-b3b3-e34cfe47b7b7
 # Define an objective function for hyperparameter optimisation
-function objective(trial, dd_prob, basis, implicit=false)
+function objective(trial, dd_prob, basis, with_implicits)
 	@unpack λ, ν = trial
-	if implicit
-		res_dd = DataDrivenDiffEq.solve(dd_prob, 
+	if with_implicits
+		res_dd = DataDrivenDiffEq.solve(
+			dd_prob, 
 			basis, 
-			ImplicitOptimizer(DataDrivenSparse.SR3()), 
+			ImplicitOptimizer(DataDrivenSparse.SR3(λ, ν)), 
 			options = options)
-
 	else
-		res_dd = DataDrivenDiffEq.solve(dd_prob, 
-			basis, DataDrivenSparse.SR3(λ, ν), 
+		res_dd = DataDrivenDiffEq.solve(
+			dd_prob, 
+			basis, 
+			DataDrivenSparse.SR3(λ, ν), 
 			options = options)
 	end
-	return res_dd.out[1].dof + log(res_dd.out[1].testerror)
+
+	push!(metric_dof, res_dd.out[1].dof)
+	push!(metric_bic, bic(res_dd))
+	return bic(res_dd)
+
 end
 
 # ╔═╡ 0f7076ef-848b-4b3c-b918-efb6419787be
-function get_best_hyperparameters(dd_prob, basis, implicit=false)
+function get_best_hyperparameters(dd_prob, basis, with_implicits)
 	
 	# Define the range of hyperparameters to consider
-	scenario = Scenario(λ = 1e-3:1e-3:5e-1, ν = exp10.(-3:1:2),
-		max_trials = 1000, 
+	scenario = Scenario(λ = 1e-2:1e-3:9e-1, ν = exp10.(-2:1:3),
+		max_trials = 300, 
 		sampler=HyperTuning.RandomSampler(), batch_size=1)
+	
+	hp_res = HyperTuning.optimize(trial -> objective(trial, dd_prob, basis, with_implicits), scenario)
 
-	hp_res = HyperTuning.optimize(trial -> objective(trial, dd_prob, basis, implicit), scenario)
 	return hp_res.best_trial.values[:λ], hp_res.best_trial.values[:ν]
 end
-
-# ╔═╡ 68b70cfc-df17-4594-9502-192ea70e5a99
-dd_prob = DataDrivenDiffEq.DirectDataDrivenProblem(
-		ngf_data.X', ngf_data.Y')
 
 # ╔═╡ 5ef43425-ca26-430c-a62d-e194a8b1aebb
 md"""
@@ -260,49 +264,37 @@ md"""
 
 # ╔═╡ c7e825a3-8ce6-48fc-86ac-21810d32bbfb
 # Bootstrapping function that estimate optimal library coefficients given data
-function sindy_bootstrap(df, basis, n_bstraps, create_dd_prob, implicit=false)
+function sindy_bootstrap(data, basis, n_bstraps)
 
-	dd_prob = create_dd_prob(df)
-	n_eqs = size(dd_prob.Y)[1]
+	n_eqs = size(data.Y, 2)
 	l_basis = length(basis)
 	bootstrap_coef = zeros(n_bstraps, n_eqs, l_basis)
 
 	for i in 1:n_bstraps
-		dd_prob = create_dd_prob(df)
-		best_λ, best_ν = get_best_hyperparameters(dd_prob, basis, implicit)
+		rand_ind = rand(1:size(data.X, 1), size(data.X, 1))
+		X = data.X[rand_ind,:]
+		Y = data.Y[rand_ind,:]
+		dd_prob = DataDrivenDiffEq.DirectDataDrivenProblem(X', Y')
 
-		#try
-		if implicit
-			best_res = DataDrivenDiffEq.solve(dd_prob, basis, ImplicitOptimizer(DataDrivenSparse.SR3(best_λ, best_ν)), options = options) 
-				#hp_res.best_trial.values[:λ], hp_res.best_trial.values[:ν])
-				#DataDrivenDiffEq.solve(dd_prob, basis, DataDrivenSparse.ImplicitOptimizer(), options=options)
+		# Check if the problem involves implicits
+		implicits = implicit_variables(basis)
+		with_implicits = false
+		if !isempty(implicits)
+			with_implicits = true
+		end
+		
+		best_λ, best_ν = get_best_hyperparameters(dd_prob, basis, with_implicits)
+		if with_implicits
+			best_res = DataDrivenDiffEq.solve(dd_prob, basis, ImplicitOptimizer(DataDrivenSparse.SR3(best_λ, best_ν)), options=options)
+
 		else
-			best_res = DataDrivenDiffEq.solve(dd_prob, basis,
-				DataDrivenSparse.SR3(best_λ, best_ν), options=options)
+			best_res = DataDrivenDiffEq.solve(dd_prob, basis, DataDrivenSparse.SR3(best_λ, best_ν), options = options) 			
+
 		end
 		bootstrap_coef[i,:,:] = best_res.out[1].coefficients
 		
 	end
 	return bootstrap_coef 
-end
-
-# ╔═╡ 79ff6845-e83c-4f15-a7e7-d692196070e3
-
-
-# ╔═╡ 83457168-c643-49d3-8e2c-f3679669ab60
-function nfb_dd_prob(df)
-	df = df[rand(1:nrow(df), nrow(df)),:]
-	return DataDrivenDiffEq.DirectDataDrivenProblem(
-		[df.g1p_fit df.g2p_fit df.Xact_fit]', t=df.time,
-		[df.NN1 df.NN2]')
-end
-
-# ╔═╡ 3194564c-6889-455e-b5f3-98d2fa0aa810
-function gf_dd_prob(df)
-	df = df[rand(1:nrow(df), nrow(df)),:]
-	return DataDrivenDiffEq.DirectDataDrivenProblem(
-		[df.Raf_fit df.PFB_fit]', df.NN_approx')
-		#[df.R_fit df.Ras_fit df.Raf_fit df.MEK_fit df.ERK_fit df.NFB_fit df.PFB_fit]', df.NN_approx')
 end
 
 # ╔═╡ 74b2ade4-884b-479d-9fee-828d37d7ab47
@@ -340,24 +332,29 @@ end
 
 # ╔═╡ 97ae69f0-764a-4aff-88cd-91accf6bb3fd
 function build_equations(coef, basis)
-	
+
 	# Build equation
 	h = [equation.rhs for equation in DataDrivenDiffEq.equations(basis)]
 	final_eqs = [sum(row .* h) for row in eachrow(coef)]
 
+	implicits = implicit_variables(basis)
+	if !isempty(implicits)
+		final_eqs = ModelingToolkit.solve_for(final_eqs, implicits)
+	end
+
 	# Build callable function and print equation
 	y = []
 	println("Estimated equation(s):")
-	for (i, eq) in enumerate(final_eqs)
-		println("y$(i) = $(eq)")
-		push!(y, Symbolics.build_function(eq, x[1:3], expression = Val{false}))
+	for (j, eq) in enumerate(final_eqs)
+		println("y$(j) = $(eq)")
+		push!(y, Symbolics.build_function(eq, [x[1:2]; i[1]], expression = Val{false}))
 	end
 	
 	return y
 end
 
 # ╔═╡ cc2395cc-00df-4e5e-8573-e85ce813fd41
-function plot_esindy(df, y, sem, basis, confidence)
+function plot_esindy(data, y, sem, basis, confidence)
 	
 	# Get the quantile corresponding to the confidence interval
 	Z = Normal()
@@ -366,33 +363,38 @@ function plot_esindy(df, y, sem, basis, confidence)
 	else
 		q = quantile(Z, confidence/100)
 	end
-	y_sem = build_equations(sem, basis)
+
+	# Obtain the equations for the SEM
+	#y_sem = build_equations(sem, basis)
 
 	# Retrieve the number of samples
-	n_samples = sum(df.time .== 0)
-	size_sample = Int(nrow(df) / n_samples)
+	n_samples = sum(data.time .== 0)
+	size_sample = Int(length(ngf_data.time) / n_samples)
 	
 	n_eqs = size(y, 1)
 	subplots = []
+	palette = :default
 	for i in 1:n_eqs
+		
 		p = plot(title="Equation $(i)")
 		for sample in 0:(n_samples-1)
 			i_start = 1 + sample * size_sample
 			i_end = i_start + size_sample - 1
-			y_vals = [y[i](x) for x in eachrow([df.g1p_fit[i_start:i_end] df.g2p_fit[i_start:i_end] df.Xact_fit[i_start:i_end]])]
 			
+			y_vals = [y[i](x) for x in eachrow([ngf_data.X[i_start:i_end,:] ngf_data.Y[i_start:i_end]])] 
 			if length(y_vals) == 1
 				y_vals = repeat([y_vals], size_sample)
 			end
-			sem_vals = [y_sem[i](x) for x in eachrow([df.g1p_fit[i_start:i_end] df.g2p_fit[i_start:i_end] df.Xact_fit[i_start:i_end]])]
 			
+			sem_vals = [y[1](x) for x in eachrow([ngf_data.X[i_start:i_end,:] ngf_data.Y[i_start:i_end]])] 
 			if length(sem_vals) == 1
 				sem_vals = repeat([sem_vals], size_sample)
 			end
-			
-			plot!(p, df.time[i_start:i_end], y_vals, label="SINDy (sample $(sample+1))", ribbon=q*sem_vals, fillalpha=0.3)
 
-			#plot!(p, df.time[1:size_sample], [])
+			plot!(p, ngf_data.time[i_start:i_end], y_vals, label="SINDy (sample $(sample+1))")#, ribbon=q*sem_vals, fillalpha=0.3)#, color=palette[i+1])
+
+			plot!(p, ngf_data.time[i_start:i_end], ngf_data.GT[i_start:i_end],
+			linestyle=:dash, label="GT")
 		end
 		push!(subplots, p)
 	end
@@ -404,10 +406,10 @@ function plot_esindy(df, y, sem, basis, confidence)
 end
 
 # ╔═╡ 012a5186-03aa-482d-bb62-ecba49587877
-function e_sindy(df, basis, dd_prob_func, n_bstrap, coef_threshold, confidence = 95)
+function e_sindy(data, basis, n_bstrap, coef_threshold, confidence = 65)
 
 	# Run sindy bootstraps
-	bootstrap_res = sindy_bootstrap(df, basis, n_bstrap, dd_prob_func)
+	bootstrap_res = sindy_bootstrap(data, basis, n_bstrap)
 
 	# Compute the mean and std of ensemble coefficients
 	e_coef, coef_sem = compute_coef_stat(bootstrap_res, coef_threshold)
@@ -426,11 +428,49 @@ md"""
 ### GFs model
 """
 
-# ╔═╡ be4ad756-f819-4e5e-8baf-2c47317d51f2
+# ╔═╡ cd655445-6bf6-4aee-8968-e324ea66601d
+res = sindy_bootstrap(ngf_data, gf_basis, 1)
+
+# ╔═╡ 689cd930-e06d-4077-a433-88430ed2079a
+begin
+	indices = sortperm(metric_dof)
+	scatter(metric_dof[indices], metric_bic[indices])
+end
+
+# ╔═╡ f27a340f-8d78-4e8a-8606-82c7e85e50fc
 # ╠═╡ disabled = true
 #=╠═╡
-res = sindy_bootstrap(df_NGF, gf_basis, 1, gf_dd_prob, true)
+res = sindy_bootstrap(ngf_data, gf_basis, 10)
   ╠═╡ =#
+
+# ╔═╡ c6e9f4ae-bb48-4f4e-88e5-165e8d1f3c8e
+e_coef, coef_sem = compute_coef_stat(res, 65)
+
+# ╔═╡ 50f61ebc-5788-403d-bb62-010abbd1e618
+begin
+	# Build equation
+	h = [equation.rhs for equation in DataDrivenDiffEq.equations(gf_basis)]
+	final_eqs = [sum(row .* h) for row in eachrow(e_coef)]
+
+	println(final_eqs[1])
+
+	implicits = implicit_variables(gf_basis)
+	final_eqs = ModelingToolkit.solve_for(final_eqs, implicits)
+	
+	# Build callable function and print equation
+	y = []
+	println("Estimated equation(s):")
+	for (j, eq) in enumerate(final_eqs)
+		println("y$(j) = $(eq)")
+		push!(y, Symbolics.build_function(final_eqs[j], [x[1:2]; i[1]], expression = Val{false}))
+	end
+end
+
+# ╔═╡ 0ef4979a-aa1a-48b4-b347-9b71d84e66b3
+p = plot_esindy(ngf_data, y, coef_sem, gf_basis, 95)
+
+# ╔═╡ f2e7f963-ba4a-44b7-a24c-313269780a0e
+plot(p[1])
 
 # ╔═╡ 77c1d355-8a7e-414e-9e0e-8eda1fbbbf1d
 md"""
@@ -534,34 +574,37 @@ esindy_res_ab.coef_mean
 # ╠═6b5db2ee-bb76-4617-b6b1-e7b6064b0fb9
 # ╟─d6b1570f-b98b-4a98-b5e5-9d747930a5ab
 # ╟─666a20eb-8395-4859-ae70-aa8ea22c5c77
-# ╠═af06c850-2e8b-4b4b-9d0f-02e645a79743
+# ╟─af06c850-2e8b-4b4b-9d0f-02e645a79743
 # ╟─b549bff5-d8e9-4f41-96d6-2d562584ccd9
-# ╠═f598564e-990b-436e-aa97-b2239b44f6d8
-# ╠═6c7929f5-15b2-4e19-8c26-e709f0da182e
-# ╠═5f0fb956-7c84-4ff2-944f-e9ddfccab748
-# ╠═a23630a3-d0e0-4f7d-9cfc-aeda021d54c1
-# ╟─5092c181-85ee-438c-9f0e-52c1f8c284dd
-# ╟─f81bdacb-f5d1-40de-9f63-29f6d8934d95
-# ╟─3b439203-21dc-42e4-91f6-57e05fa3bc12
+# ╟─f598564e-990b-436e-aa97-b2239b44f6d8
+# ╟─f21876f0-3124-40ad-ad53-3a53efe77040
+# ╟─6c7929f5-15b2-4e19-8c26-e709f0da182e
+# ╟─5f0fb956-7c84-4ff2-944f-e9ddfccab748
 # ╟─74ad0ae0-4406-4326-822a-8f3e027077b3
 # ╟─9dc0a251-a637-4144-b32a-7ebf5b86b6f3
 # ╟─ede9d54f-aaa4-4ff3-9519-14e8d32bc17f
 # ╟─e81310b5-63e1-45b8-ba4f-81751d0fcc06
 # ╠═4646bfce-f8cc-4d24-b461-753daff50f71
+# ╠═9c4ade80-d5ad-40b4-bf57-53479bb8a74b
+# ╠═e488c35e-d454-4942-b08e-599d27f00e17
+# ╠═1ec3d1b5-83d9-4093-8c1f-e726441743a6
+# ╠═d7ffe014-f4bf-4236-a553-d0dd6f06f146
 # ╠═034f5422-7259-42b8-b3b3-e34cfe47b7b7
 # ╠═0f7076ef-848b-4b3c-b918-efb6419787be
-# ╠═68b70cfc-df17-4594-9502-192ea70e5a99
 # ╟─5ef43425-ca26-430c-a62d-e194a8b1aebb
 # ╠═c7e825a3-8ce6-48fc-86ac-21810d32bbfb
-# ╠═79ff6845-e83c-4f15-a7e7-d692196070e3
-# ╟─83457168-c643-49d3-8e2c-f3679669ab60
-# ╟─3194564c-6889-455e-b5f3-98d2fa0aa810
 # ╟─74b2ade4-884b-479d-9fee-828d37d7ab47
 # ╟─97ae69f0-764a-4aff-88cd-91accf6bb3fd
 # ╠═cc2395cc-00df-4e5e-8573-e85ce813fd41
 # ╠═012a5186-03aa-482d-bb62-ecba49587877
 # ╟─569c7600-246b-4a64-bba5-1e74a5888d8c
-# ╠═be4ad756-f819-4e5e-8baf-2c47317d51f2
+# ╠═cd655445-6bf6-4aee-8968-e324ea66601d
+# ╟─689cd930-e06d-4077-a433-88430ed2079a
+# ╠═f27a340f-8d78-4e8a-8606-82c7e85e50fc
+# ╠═c6e9f4ae-bb48-4f4e-88e5-165e8d1f3c8e
+# ╠═50f61ebc-5788-403d-bb62-010abbd1e618
+# ╠═0ef4979a-aa1a-48b4-b347-9b71d84e66b3
+# ╠═f2e7f963-ba4a-44b7-a24c-313269780a0e
 # ╟─77c1d355-8a7e-414e-9e0e-8eda1fbbbf1d
 # ╟─f3077d61-cb49-4efb-aac0-66e8de6e15ae
 # ╟─d10c9991-ada5-4239-8b1a-5d9baaf27a1a
