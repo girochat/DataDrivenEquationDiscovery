13bd96650f21bfdf395e7e618aa06ba72d54b3e2
girochat <giliane.rochat@students.unibe.ch>
2024-10-27 22:01:34 +0100

    NN parameters for sustained prediction

--- RBF.jl
+++ RBF.jl
@@ -3,11 +3,11 @@ module RBF
 export rbf
 export tanh
 
-function rbf(x)
-	return exp.(-x .^ 2)
-end
+    function rbf(x)
+	    return exp.(-x .^ 2)
+    end
 
-function tanh(x)
-	return tanh.(x)
-
+    function mytanh(x)
+	    return tanh.(x)
+    end
 end
